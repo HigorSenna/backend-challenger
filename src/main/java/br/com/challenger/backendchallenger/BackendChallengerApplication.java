3aa14bac18f1a746d8b77e71b0450074b2da3723
Higor Senna <higorrebjfmg@gmail.com>
2019-10-02 19:45:47 -0300

    Inicializado projeto

---
+++ src/main/java/br/com/challenger/backendchallenger/BackendChallengerApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenger.backendchallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengerApplication.class, args);
+	}
+
+}
